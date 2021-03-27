6a67db36fba51873ababef0c9bd1e45f659cd879
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-27 21:47:31 +0900

    Added fetch products hook in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products';
+import { commerce } from './lib/commerce';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // use useEffects to load the products
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    // data is already destructured. and set it to the component state
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
