4f583ee186a75827471d72685e2297ce4d7e1e3a
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-29 15:16:07 +0900

    Added CarItems component

---
+++ src/components/Cart/CarItem/CartItem.js
@@ -0,0 +1,27 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core';
+import React from 'react';
+import useStyles from './styles';
+
+const CartItem = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography />
+      <Button />
+      <Card />
+      <CardActions />
+      <CardContent></CardContent>
+      <CardMedia />
+    </div>
+  );
+};
+
+export default CartItem;
