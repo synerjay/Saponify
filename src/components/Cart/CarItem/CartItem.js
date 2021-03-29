9e398c50422778296cbf03c05e03ca477ad01c5d
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-29 15:23:23 +0900

    Added cards in CartItem

--- src/components/Cart/CarItem/CartItem.js
+++ src/components/Cart/CarItem/CartItem.js
@@ -9,17 +9,25 @@ import {
 import React from 'react';
 import useStyles from './styles';
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
   const classes = useStyles();
 
   return (
     <div>
-      <Typography />
-      <Button />
-      <Card />
-      <CardActions />
-      <CardContent></CardContent>
-      <CardMedia />
+      <Card>
+        <CardMedia image={item.media.source} alt={item.name} className={class.media} />
+        <CardContent className={classes.cardContent}>
+          <Typography variant="h4">{item.name}</Typography>
+          <Typography variant="h5">{item.line_total.formatted_wth_symbol}</Typography>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <div className={classes.buttons}>
+            <Button type="button" size="small">-</Button>
+            <Typography>{item.quantity}</Typography>
+            <Button type="button" size="small">+</Button>
+          </div>
+        </CardActions>
+      </Card>
     </div>
   );
 };
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import { Button, Container, Grid, Typography } from '@material-ui/core';
 import React, { Fragment } from 'react';
+import CartItem from './CarItem/CartItem';
 import useStyles from './styles';
 
 const Cart = ({ cart }) => {
@@ -17,7 +18,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} />
           </Grid>
         ))}
       </Grid>
