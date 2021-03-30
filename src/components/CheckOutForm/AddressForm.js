2807d9a35f269f6d81441b7637680a185c0b995f
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-30 17:55:28 +0900

    Added form input component

--- src/components/CheckOutForm/AddressForm.js
+++ src/components/CheckOutForm/AddressForm.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import {
+  Button,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import FormInput from './CustomTextField';
 
 const AddressForm = () => {
-  return <div>Address Form</div>;
+  const methods = useForm();
+
+  return (
+    <Fragment>
+      <Typography variant='h6' gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form>
+          <Grid container spacing={3}>
+            <FormInput required name='firstName' label='First name' />
+            <FormInput required name='lastName' label='Last name' />
+            <FormInput required name='address1' label='Address' />
+            <FormInput required name='email' label='Email' />
+            <FormInput required name='city' label='City' />
+            <FormInput required name='postal' label='ZIP / Postal Code' />
+          </Grid>
+        </form>
+      </FormProvider>
+    </Fragment>
+  );
 };
 
 export default AddressForm;
