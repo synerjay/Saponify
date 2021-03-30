217176e4849317923b6f0fa5441ba404c7843589
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-30 17:29:43 +0900

    Added addressform paymentform components under checkout component

--- src/components/CheckOutForm/Checkout/Checkout.js
+++ src/components/CheckOutForm/Checkout/Checkout.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import useStyles from './styles';
+import {
+  Button,
+  CircularProgress,
+  Divider,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from '@material-ui/core';
+import React, { Fragment, useState } from 'react';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+
+const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
-  return <div></div>;
+  const [activeStep, setActiveStep] = useState(2);
+  const classes = useStyles();
+
+  const Confirmation = () => <div>Confirmation</div>;
+
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
+  return (
+    <Fragment>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant='h4' align='center'>
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </Fragment>
+  );
 };
 
 export default Checkout;
