ffac68b2a31ea1a13220557066dbf6d750adfb36
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-02 13:44:11 +0900

    Added subdivisions

--- src/components/CheckOutForm/AddressForm.js
+++ src/components/CheckOutForm/AddressForm.js
@@ -17,19 +17,25 @@ const AddressForm = ({ checkoutToken }) => {
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState('');
   const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
-  const [shippingSubdivion, setShippingSubdivion] = useState('');
+  const [shippingSubdivision, setShippingSubdivision] = useState('');
   const [shippingOptions, setShippingOptions] = useState([]);
   const [shippingOption, setShippingOption] = useState('');
 
-  console.log(shippingCountries);
+  console.log(shippingCountry);
 
   // Make To make everything from object to an Array object
   // Object.entries() method returns an array an array [[key, value], [key,value]]
   // because now shippingCountries is an array, we can use map method to make it an array of objects
-  const countries = Object.entries(
-    shippingCountries
-  ).map(([code, country]) => ({ id: code, label: country }));
+  const countries = Object.entries(shippingCountries).map(([code, name]) => ({
+    id: code,
+    label: name,
+  }));
+  const subdivisions = Object.entries(
+    shippingSubdivisions
+  ).map(([code, name]) => ({ id: code, label: name }));
+
   console.log(countries);
+  console.log(subdivisions);
 
   const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries } = await commerce.services.localeListShippingCountries(
@@ -38,8 +44,17 @@ const AddressForm = ({ checkoutToken }) => {
 
     console.log(countries);
     setShippingCountries(countries);
-    setShippingCountry(Object.keys(countries)[0]);
+    setShippingCountry(Object.keys(countries)[0]); // initial state is the first country in the list
     // Object.keys(object) returns an array of Keys
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(
+      countryCode
+    );
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
   };
 
   // Component to mount so that it fetches the available countries from Commerce.js server
@@ -47,6 +62,10 @@ const AddressForm = ({ checkoutToken }) => {
   useEffect(() => {
     fetchShippingCountries(checkoutToken.id);
   }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
 
   return (
     <Fragment>
@@ -76,13 +95,21 @@ const AddressForm = ({ checkoutToken }) => {
                 ))}
               </Select>
             </Grid>
-            {/* <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Subdivisions</InputLabel>
-              <Select fullWidth>
-                <MenuItem>Select Me</MenuItem>
+              <Select
+                value={shippingSubdivision}
+                fullWidth
+                onChange={(e) => setShippingSubdivision(e.target.value)}
+              >
+                {subdivisions.map((subdivision) => (
+                  <MenuItem key={subdivision.id} value={subdivision.id}>
+                    {subdivision.label}
+                  </MenuItem>
+                ))}
               </Select>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Options</InputLabel>
               <Select fullWidth>
                 <MenuItem>Select Me</MenuItem>
