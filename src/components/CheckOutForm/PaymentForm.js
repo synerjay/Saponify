f9687dfec4ed9041ba702128d2108c21e87fb45d
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-02 20:20:50 +0900

    Added stripe payment option

--- src/components/CheckOutForm/PaymentForm.js
+++ src/components/CheckOutForm/PaymentForm.js
@@ -4,16 +4,47 @@ import {
   CardElement,
   ElementsConsumer,
 } from '@stripe/react-stripe-js';
-import { loadStrip } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React, { Fragment } from 'react';
 import Review from './Review';
 
+const stripePromise = loadStripe('...');
+
 // We use Stripe.js to use the credit card payments
-const PaymentForm = () => {
+const PaymentForm = ({ checkoutToken, backStep }) => {
   return (
     <Fragment>
       {/* Put review component here */}
-      <Review />
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant='h6' gutterBottom style={{ margin: '20px 0' }}>
+        {' '}
+        Payment Method{' '}
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>
+          {({ elements, stripe }) => (
+            <form>
+              <CardElement />
+              <br />
+              <br />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button variant='outlined' onClick={backStep}>
+                  Back
+                </Button>
+                <Button
+                  type='submit'
+                  variant='contained'
+                  disabled={!stripe}
+                  color='primary'
+                >
+                  Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                </Button>
+              </div>
+            </form>
+          )}
+        </ElementsConsumer>
+      </Elements>
     </Fragment>
   );
 };
