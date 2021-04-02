577431e81e53f2dd9f4db100e9ae052d3b211c54
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-02 20:50:38 +0900

    Added stripe token in payment method component

--- src/components/CheckOutForm/PaymentForm.js
+++ src/components/CheckOutForm/PaymentForm.js
@@ -8,10 +8,50 @@ import { loadStripe } from '@stripe/stripe-js';
 import React, { Fragment } from 'react';
 import Review from './Review';
 
-const stripePromise = loadStripe('...');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 // We use Stripe.js to use the credit card payments
-const PaymentForm = ({ checkoutToken, backStep }) => {
+const PaymentForm = ({ checkoutToken, backStep, shippingData }) => {
+  const handleSubmit = async (event, elements, stripe) => {
+    //prevents the site to refresh when submitted
+    event.preventDefault();
+
+    // if there are no stripe and elements then dont do handleSubmit
+    if (!stripe || !elements) return;
+    const cardElement = elements.getElement(CardElement);
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log(error);
+    } else {
+      const orderData = {
+        line_items: checkoutToken.live.line_items,
+        customer: {
+          firstname: shippingData.firstName,
+          lastname: shippingData.lastName,
+          email: shippingData.email,
+        },
+        shipping: {
+          name: 'International',
+          street: shippingData.address1,
+          town_city: shippingData.city,
+          county_state: shippingData.shippingSubdivision,
+          postal_zip_code: shippingData.zip,
+          country: shippingData.shippingCountry,
+        },
+        fulfillment: { shipping_method: shippingData.shippingOption },
+        payment: {
+          gateway: 'stripe',
+          stripe: {
+            payment_method_id: paymentMethod.id,
+          },
+        },
+      };
+    }
+  };
   return (
     <Fragment>
       {/* Put review component here */}
@@ -24,7 +64,7 @@ const PaymentForm = ({ checkoutToken, backStep }) => {
       <Elements stripe={stripePromise}>
         <ElementsConsumer>
           {({ elements, stripe }) => (
-            <form>
+            <form onSubmit={(e) => handleSubmit(e, elements, stripe)}>
               <CardElement />
               <br />
               <br />
