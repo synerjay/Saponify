5b859c86d323e2da8803b2064d7bfc4e0a90d1f2
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-10 21:46:53 +0900

    Adjusted the padding

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,7 +14,7 @@ const Products = ({ products, onAddToCart, emptyErrors }) => {
   return (
     <main className='flex flex-col justify-center mt-16 md:mt-16'>
       <CarouselSlide />
-      <div className='mt-2 p-3 md:p-16 grid gap-10 md:grid-cols-3'>
+      <div className='mt-2 p-5 md:p-16 grid gap-10 md:grid-cols-3'>
         {products.map((item) => (
           <Product item={item} onAddToCart={onAddToCart} />
         ))}
