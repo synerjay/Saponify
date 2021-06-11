89374aafc0200276b7a7bb6cd6e52d7e683a405e
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-11 16:02:16 +0900

    Removed unnecessary code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,4 @@
-// import {
-//   Card,
-//   CardActions,
-//   CardContent,
-//   CardMedia,
-//   IconButton,
-//   Typography,
-// } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-
-// import useStyles from './styles';
 
 import React from 'react';
 
@@ -53,45 +43,3 @@ const Product = ({ item, onAddToCart }) => {
 };
 
 export default Product;
-
-// const Product = ({ item, onAddToCart }) => {
-//   const classes = useStyles();
-
-//   console.log(item);
-
-//   return (
-//     <div>
-//       <Card className={classes.root}>
-//         <CardMedia
-//           className={classes.media}
-//           image={item.media.source}
-//           title={item.name}
-//         />
-//         <CardContent>
-//           <div className={classes.cardContent}>
-//             <Typography variant='h6' gutterBottom>
-//               {item.name}
-//             </Typography>
-//             <Typography variant='h6'>
-//               {item.price.formatted_with_symbol}
-//             </Typography>
-//           </div>
-//           <Typography
-//             dangerouslySetInnerHTML={{ __html: item.description }}
-//             noWrap
-//             variant='body2'
-//             color='textSecondary'
-//           />
-//         </CardContent>
-//         <CardActions disableSpacing className={classes.cardActions}>
-//           <IconButton
-//             aria-label='Add to Cart'
-//             onClick={() => onAddToCart(item.id, 1)}
-//           >
-//             <AddShoppingCart />
-//           </IconButton>
-//         </CardActions>
-//       </Card>
-//     </div>
-//   );
-// };
