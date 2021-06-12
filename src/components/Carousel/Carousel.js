98f55ce815abea153690321342fb80d7d2d9033f
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-12 16:04:11 +0900

    Added soap carousel images to component

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
+import carousel1 from '../../assets/carouselimages/carousel1.jpeg';
+import carousel2 from '../../assets/carouselimages/carousel2.jpeg';
+import carousel3 from '../../assets/carouselimages/carousel3.jpeg';
+import carousel4 from '../../assets/carouselimages/carousel4.jpeg';
+import carousel5 from '../../assets/carouselimages/carousel5.jpeg';
 
 function CarouselSlide() {
   // API to get images: https://source.unsplash.com/1024x400/?fashion
@@ -19,25 +24,19 @@ function CarouselSlide() {
       width='100%'
     >
       <div>
-        <img
-          className='h-4/6'
-          alt=''
-          src='https://images.unsplash.com/photo-1572577892547-18d319fcece6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIzMzI3OTUx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024'
-        />
+        <img className='h-4/6' alt='' src={carousel1} />
       </div>
       <div>
-        <img
-          className='h-4/6'
-          alt=''
-          src='https://images.unsplash.com/photo-1537832816519-689ad163238b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIzMzI4MDU4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024'
-        />
+        <img className='h-4/6' alt='' src={carousel2} />
       </div>
       <div>
-        <img
-          className='h-4/6'
-          alt=''
-          src='https://images.unsplash.com/photo-1561972793-b361046d7b3a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjIzMzI4MDkw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1024'
-        />
+        <img className='h-4/6' alt='' src={carousel3} />
+      </div>
+      <div>
+        <img className='h-4/6' alt='' src={carousel4} />
+      </div>
+      <div>
+        <img className='h-4/6' alt='' src={carousel5} />
       </div>
     </Carousel>
   );
