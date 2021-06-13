39077861d0900abbb2a035c82a988c7a12c34e65
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-13 16:02:30 +0900

    added the appropriate icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faShippingFast } from '@fortawesome/free-solid-svg-icons';
+import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeadset } from '@fortawesome/free-solid-svg-icons';
 
 function Services() {
   return (
     <div className='flex flex-row w-screen justify-evenly'>
       <div className='service'>
-        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faShippingFast} />
       </div>
       <div className='service'>
-        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faHandHoldingHeart} />
       </div>
       <div className='service'>
-        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faHeadset} />
       </div>
     </div>
   );
