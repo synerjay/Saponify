db46fda26614f6ca341f84b35d92cb1c8394cd24
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-13 15:38:31 +0900

    made spaces in each services

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,15 +4,15 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function Services() {
   return (
-    <div className='flex flex-row w-screen justify-center'>
+    <div className='flex flex-row w-screen justify-evenly'>
       <div className='service'>
-        <FontAwesomeIcon icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
       </div>
       <div className='service'>
-        <FontAwesomeIcon icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
       </div>
       <div className='service'>
-        <FontAwesomeIcon icon={faCoffee} />
+        <FontAwesomeIcon className='text-8xl' icon={faCoffee} />
       </div>
     </div>
   );
