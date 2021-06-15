513739e1428633a114d26be1a164925db0e8f024
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-15 19:24:25 +0900

    Make cart icon disappear when client in the cart page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -108,31 +108,34 @@ const Navbar = ({ totalItems }) => {
             </div>
           </div>
           <div className='flex justify-center md:block'>
-            <Link
-              className='relative inline-block text-gray-700 hover:text-gray-600'
-              to='/cart'
-            >
-              <svg
-                className='h-5 w-5'
-                viewBox='0 0 24 24'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
+            {location.pathname === '/cart' ||
+            location.pathname === '/checkout' ? null : (
+              <Link
+                className='relative inline-block text-gray-700 hover:text-gray-600'
+                to='/cart'
               >
-                <path
-                  d='M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z'
-                  stroke='currentColor'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                />
-              </svg>
+                <svg
+                  className='h-5 w-5'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z'
+                    stroke='currentColor'
+                    stroke-width='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                </svg>
 
-              {totalItems === 0 ? null : (
-                <span class='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
-                  {totalItems}
-                </span>
-              )}
-            </Link>
+                {totalItems === 0 ? null : (
+                  <span class='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
+                    {totalItems}
+                  </span>
+                )}
+              </Link>
+            )}
           </div>
         </div>
 
