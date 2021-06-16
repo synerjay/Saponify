ac95bf2fddf53320744e7e0c4a8461c5904f990f
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-16 21:49:26 +0900

    Cleaning up unnecessary code

--- src/components/Cart/CarItem/CartItem.js
+++ src/components/Cart/CarItem/CartItem.js
@@ -1,27 +1,6 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-} from '@material-ui/core';
 import React from 'react';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import useStyles from './styles';
 
 const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
-  const classes = useStyles();
-
-  // IMAGE: item.media.source
-  // ITEM NAME: item.name
-  // EACH ITEM TOTAL: item.line_total.formatted_with_symbol
-  // DECREMENT BUTTON: onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
-  // ITEM QUANTITY: item.quantity
-  // INCREMENT BUTTON: onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
-  // REMOVE ITEM FROM CART: onClick={() => handleRemoveFromCart(item.id)}
-
   return (
     <div className='card hover:shadow-xl'>
       {/* Image here */}
@@ -38,7 +17,6 @@ const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
               Quantity:
             </label>
             <div className='flex items-center mt-1'>
-              {/* DECREMENT here */}
               <button
                 onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
                 className='text-gray-500 focus:outline-none focus:text-gray-600'
@@ -106,46 +84,3 @@ const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
 };
 
 export default CartItem;
-
-{
-  /* <div>
-<Card>
-  <CardMedia
-    image={item.media.source}
-    alt={item.name}
-    className={classes.media}
-  />
-  <CardContent className={classes.cardContent}>
-    <Typography variant='h5'>{item.name}</Typography>
-    <Typography variant='h5'>
-      {item.line_total.formatted_with_symbol}
-    </Typography>
-  </CardContent>
-  <CardActions className={classes.cartActions}>
-    <div className={classes.buttons}>
-      <Button
-        type='button'
-        onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
-      >
-        <RemoveIcon />
-      </Button>
-      <Typography variant='h4'>{item.quantity}</Typography>
-      <Button
-        type='button'
-        onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
-      >
-        <AddIcon />
-      </Button>
-    </div>
-    <Button
-      variant='contained'
-      type='button'
-      color='secondary'
-      onClick={() => handleRemoveFromCart(item.id)}
-    >
-      Remove Item
-    </Button>
-  </CardActions>
-</Card>
-</div> */
-}
